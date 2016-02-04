5105cc53fb16a5ede48c3fd7e18cf3ed423f3cfe
zgriesinger <zgriesinger@gmail.com>
2016-02-04 16:51:03 -0600

    Works now. Run spotify.py then index.py should work need to make sure that people can input stuff.

--- index.py
+++ index.py
@@ -19,27 +19,30 @@ def parse_file(filename):
             playlist_temp.append(i.strip("\n"))
         else:
             i = i.strip("\n")
-            print i
             song_artist = i.split(" - "),
             playlist_temp.append(song_artist)
     playlists_and_songs.remove([])
     return playlists_and_songs
 
 
-all_playlists = parse_file("spotify_playlists.txt")
-print(all_playlists)
-
+all_playlists = parse_file("playlists.txt")
 for i in all_playlists:
     print i
-    if i[0][0:2] == "P: ":
-        current_playlist = api.create_playlist(i[0].strip("P: "))
-    else:
-        current_song = i[1][0] + " " + i[1][1]
+    for j in i:
+        if j[0:2] == "P:":
+            print "Creating playlist: " + j[3:]
+            current_playlist = api.create_playlist(j[3:])
+        else:
+            current_song = j[0][0] + " " + j[0][1]
 
-        search_current_song = api.search_all_access(current_song, 1)
-        print search_current_song
-        current_song_id = current_song[u'song_hits'][u'track'][u'storeId']
-        api.add_songs_to_playlist(current_playlist, current_song_id)
+            search_current_song = api.search_all_access(current_song, 1)
+            if search_current_song['song_hits'] == []:
+                print "No results found for " + current_song
+                continue
+            current_song_id = search_current_song['song_hits'][0]['track']['storeId']
+
+            print current_song_id
+            api.add_songs_to_playlist(current_playlist, current_song_id)
 
 ##desperado = api.search_all_access("Desperado - The Eagles", 1)
 ##print(desperado['song_hits'])
--- spotify.py
+++ spotify.py
@@ -1,14 +1,20 @@
 # shows a user's playlists (need to be authenticated via oauth)
 # MY USER 1240630908
+# -*- coding: utf-8 -*-
 import sys
 import os
 import spotipy
 import spotipy.util as util
+import io
 
 def show_tracks(results):
     for i, item in enumerate(tracks['items']):
         track = item['track']
         print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
+        output_song = track['name'] + ' - ' + track['artists'][0]['name'] + '\n'
+        output_song_final = unicode(output_song)
+        type(output_song_final)
+        output_file.write(output_song_final)
 
 
 if __name__ == '__main__':
@@ -24,6 +30,9 @@ if __name__ == '__main__':
     client_secret='cda48ec16700468ab184975dcbeb3487',
     redirect_uri='https://google.com')
 
+    output_file = io.open('playlists.txt', 'w', encoding='utf8')
+
+
     if token:
         top = 40
         sp = spotipy.Spotify(auth=token)
@@ -32,6 +41,9 @@ if __name__ == '__main__':
             if playlist['owner']['id'] == username:
                 print()
                 print(playlist['name'])
+                output_playlist_name = 'P: ' + str(playlist['name']) + '\n'
+                output_playlist_name_utf = unicode(output_playlist_name)
+                output_file.write(output_playlist_name_utf)
                 print('  total tracks', playlist['tracks']['total'])
                 results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
                 tracks = results['tracks']
